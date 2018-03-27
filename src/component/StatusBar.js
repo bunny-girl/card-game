51e93d39f2b54648a42e7a7f4166ecca6028421e
Edel Ma <qun_ma@apple.com>
2018-03-27 11:52:22 +0800

    Add a style for status and toolbar.

--- src/component/StatusBar.js
+++ src/component/StatusBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const StatusBar = () => (
-    <div className="row">
+    <div className="row status-bar">
         <div className="col-lg-1">卡牌:</div>
     </div>
 );
--- src/component/ToolBar.js
+++ src/component/ToolBar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const ToolBar = ({tools, current, moveScene}) => (
     <div className="fixed-bottom row text-center">
-        {tools.map(t => <div className="col-lg-1 col-md-2 col-sm-3" onClick={() => moveScene(t.id)}
-                             key={t.id}>{t.name}</div>)}
+        {tools.map(t => <div className="col-lg-1 col-md-2 col-sm-3 toolbar" onClick={() => moveScene(t.id)}
+                             key={t.id}><button className="btn btn-primary">{t.name}</button></div>)}
     </div>
 );
 
